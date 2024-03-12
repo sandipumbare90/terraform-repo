67e622bbcb939900ab72a9ec6774e6ffa2cecb34
Ubuntu <ubuntu@jenkins-master-server.us-central1-a.c.devops-engineer-123.internal>
2024-03-12 10:03:46 +0000

    label added

--- instance.tf
+++ instance.tf
@@ -2,6 +2,8 @@ resource "google_compute_instance" "terraform-instance" {
     name = "terraform-instance"
     machine_type = "f1-micro"
     zone = "us-central1-c"
+    labels = {
+        demo = "CI/CD"
     boot_disk {
       initialize_params{
       image = "debian-cloud/debian-11"

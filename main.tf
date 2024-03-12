c3ed9522cb33517283a1a3ebd399fb1cb34641ac
sandipumbare90 <160105096+sandipumbare90@users.noreply.github.com>
2024-03-12 13:22:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.20.0"
+    }
+  }
+}
+
+provider "google" {
+        project = devops-engineer-123
+        region = us-central1
+}

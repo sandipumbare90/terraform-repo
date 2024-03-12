8ad87151c8218641e53bdf5a86e3eacf4c5b0aa3
Ubuntu <ubuntu@jenkins-master-server.us-central1-a.c.devops-engineer-123.internal>
2024-03-12 10:19:26 +0000

    changes 2 done in file

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "google_compute_instance" "terraform-instance" {
     machine_type = "f1-micro"
     zone = "us-central1-c"
     labels = {
-        demo = "cd/cd"
+        demo = "evo"
        }
     boot_disk {
       initialize_params{

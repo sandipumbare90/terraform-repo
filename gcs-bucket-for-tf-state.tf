a9dbcc6599e787079d8b5ff661d845985c15f387
Ubuntu <ubuntu@jenkins-master-server.us-central1-a.c.devops-engineer-123.internal>
2024-03-13 05:08:48 +0000

    added hash iin gcs-bucket-for-tf-state.tf

--- gcs-bucket-for-tf-state.tf
+++ gcs-bucket-for-tf-state.tf
@@ -1,10 +1,10 @@
 #Terraform state in a Cloud Storage bucket
-backend "gcs" {
+#backend "gcs" {
    bucket  = "gcs-bucket-for-tf-state-file"
    prefix  = "terraform/state"
 }
 
-resource "google_storage_bucket" "gcs-bucket-for-tf-state-file" {
+#resource "google_storage_bucket" "gcs-bucket-for-tf-state-file" {
         name = "gcs-bucket-for-tf-state-file"
         location = "US"
         force_destroy = "true"

2f148504a06dcede684f5cdf62089ae3272184f1
Ubuntu <ubuntu@jenkins-master-server.us-central1-a.c.devops-engineer-123.internal>
2024-03-14 11:21:16 +0000

    more chnages

--- instance.tf
+++ instance.tf
@@ -3,7 +3,7 @@ resource "google_compute_instance" "terraform-instance" {
     machine_type = "f1-micro"
     zone = "us-central1-c"
     label = {
-      dept = "evo"
+      dept = "evo-sl"
 	}
     boot_disk {
       initialize_params{

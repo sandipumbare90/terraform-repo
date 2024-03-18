470d0ba8e99914fd08b237637e9fb4df981c899c
Jenkins <jenkins@control-node-for-jenkins-and-ansible.us-central1-a.c.devops-engineer-123.internal>
2024-03-18 06:51:34 +0000

    backend bucket details added

--- main.tf
+++ main.tf
@@ -3,6 +3,11 @@ terraform {
     google = {
       source = "hashicorp/google"
       version = "5.20.0"
+    backend "gcs" {
+     bucket  = "gcs-bucket-for-tf-state-file"
+     prefix  = "terraform/state"
+     /*depends_on = "${google_storage_bucket.gcs-bucket-for-tf-state-file.name}"*/
+      }
     }
   }
 }
@@ -12,4 +17,17 @@ provider "google" {
         region = "us-central1"
 	impersonate_service_account = "jenkins-cicd-sa@devops-engineer-123.iam.gserviceaccount.com"
 }
+
+resource "google_storage_bucket" "gcs-bucket-for-tf-state-file" {
+        name = "gcs-bucket-for-tf-state-file"
+        location = "US"
+        force_destroy = "true"
+        retention_policy {
+          retention_period = "2,147,483,647"
+    }
+        versioning {
+          enabled = true
+   }
+}
+
 

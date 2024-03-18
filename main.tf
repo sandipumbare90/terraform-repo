5324db7c1e4c7b1312c1aa200d52092f6902929a
Jenkins <jenkins@control-node-for-jenkins-and-ansible.us-central1-a.c.devops-engineer-123.internal>
2024-03-18 16:19:39 +0000

    new code for gcp infra provisioning

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ terraform {
 provider "google" {
         project = "devops-engineer-123"
         region = "us-central1"
-	impersonate_service_account = "jenkins-cicd-sa@devops-engineer-123.iam.gserviceaccount.com"
+	impersonate_service_account = "jenkins-sa-for-cicd@devops-engineer-123.iam.gserviceaccount.com"
 }
 
 

dcee28c719fd7d86a7491837779cddd35d850aa1
Jenkins <jenkins@control-node-for-jenkins-and-ansible.us-central1-a.c.devops-engineer-123.internal>
2024-03-18 16:31:42 +0000

    retention period removed in gcs-file

--- gcs-bucket-for-tf-state.tf
+++ gcs-bucket-for-tf-state.tf
@@ -2,10 +2,4 @@ resource "google_storage_bucket" "gcs-bucket" {
         name = "gcs-bucket"
         location = "US"
         force_destroy = "true"
-        retention_policy {
-          retention_period = "2,147,483,647"
-    }
-        versioning {
-          enabled = true
-   }
 }

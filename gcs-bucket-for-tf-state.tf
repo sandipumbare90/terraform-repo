da9242fd1b242c55ce23a8751b61b5491d63f26d
Jenkins <jenkins@control-node-for-jenkins-and-ansible.us-central1-a.c.devops-engineer-123.internal>
2024-03-18 17:20:09 +0000

    bucket name updated

--- gcs-bucket-for-tf-state.tf
+++ gcs-bucket-for-tf-state.tf
@@ -1,5 +1,5 @@
-resource "google_storage_bucket" "gcs-bucket" {
-        name = "gcs-bucket"
+resource "google_storage_bucket" "gcs-bucket-for-backend-state-files" {
+        name = "gcs-bucket-for-backend-state-files"
         location = "US"
         force_destroy = "true"
 }

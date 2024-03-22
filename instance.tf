5cf9532b407a914babf2eccc8c158558298d057d
Jenkins <jenkins@control-node-for-jenkins-and-ansible.us-central1-a.c.devops-engineer-123.internal>
2024-03-22 11:10:02 +0000

    snapshot policy added

--- instance.tf
+++ instance.tf
@@ -36,3 +36,26 @@ resource "google_compute_attached_disk" "default" {
     zone = "us-central1-c"
 }
 
+resource "google_compute_resource_policy" "my-first-compute-policy-for-snapshots" {
+    name = "my-first-compute-policy-for-snapshots"
+    region = "us-central1"
+    snapshot_schedule_policy {
+      schedule {
+        daily_schedule {
+          start_time = "08:00"
+          days_in_cycle = 1
+}
+}
+     retention_policy {
+       max_retention_days = 5
+       on_source_disk_delete = "KEEP_AUTO_SNAPSHOTS"
+}
+}
+}
+
+resource "google_compute_disk_resource_policy_attachment" "my-first-disk-attachment-policy" {
+     name = google_compute_resource_policy.my-first-compute-policy-for-snapshots.name
+     disk = google_compute_disk.disk-for-instance.name
+     
+
+}

2be61d7598e656461af40aaa97e512c1534778c3
sandipumbare90 <160105096+sandipumbare90@users.noreply.github.com>
2024-03-29 12:48:07 +0530

    Delete gcs-bucket-for-tf-state.tf

--- gcs-bucket-for-tf-state.tf
+++
@@ -1,9 +0,0 @@
-resource "google_storage_bucket" "gcs-bucket-for-backend-state-files" {
-        name = "gcs-bucket-for-backend-state-files"
-        location = "US"
-        force_destroy = "true"
-        uniform_bucket_level_access = true
-	versioning {
- 	enabled = true 
- }
-}

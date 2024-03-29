f6d8ba28183d0886b6b039007b399ea46f41f4d3
Jenkins <jenkins@control-node.us-central1-a.c.devops-engineer-123.internal>
2024-03-29 09:04:08 +0000

    sa impersonation added

--- modules/vpc/backend.tf
+++ modules/vpc/backend.tf
@@ -5,10 +5,9 @@ terraform {
       version = "5.22.0"
     }
   }
+  impersonate_service_account = "jenkins-sa-cicd@devops-engineer-123.iam.gserviceaccount.com"
   experiments = [module_variable_optional_attrs]
   backend "gcs" {
     bucket = "gcs-bucket-for-backend-state-files"
     prefix = "terraform-1/state"
 }
-}
-

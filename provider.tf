996c409a2f027ac74bee12be4b2171ca0c5d884e
Jenkins <jenkins@control-node.us-central1-a.c.devops-engineer-123.internal>
2024-03-29 09:55:34 +0000

    new changes

--- provider.tf
+++ provider.tf
@@ -1,20 +1,12 @@
 terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "5.22.0"
-    }
-  }
-  experiments = [module_variable_optional_attrs]
-/*  backend "gcs" {
+backend "gcs" {
     bucket = "gcs-bucket-for-backend-state-files"
     prefix = "terraform-1/state"
     impersonate_service_account = "jenkins-sa-cicd@devops-engineer-123.iam.gserviceaccount.com"
-}*/
 }
-
-/*provider "google" {
+}
+provider "google" {
   project = "devops-engineer-123"
   region = "us-central1"
   impersonate_service_account = "jenkins-sa-cicd@devops-engineer-123.iam.gserviceaccount.com"
-}*/
+}

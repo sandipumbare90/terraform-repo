df3fe7231efa5db74f31d0b785f38035786b0ab1
Jenkins <jenkins@control-node.us-central1-a.c.devops-engineer-123.internal>
2024-03-29 09:19:10 +0000

    google provider details added

--- modules/vpc/backend.tf
+++ modules/vpc/backend.tf
@@ -5,10 +5,15 @@ terraform {
       version = "5.22.0"
     }
   }
-  impersonate_service_account = "jenkins-sa-cicd@devops-engineer-123.iam.gserviceaccount.com"
   experiments = [module_variable_optional_attrs]
   backend "gcs" {
     bucket = "gcs-bucket-for-backend-state-files"
     prefix = "terraform-1/state"
 }
+}
+
+provider "google" {
+  project = "devops-engineer-123"
+  region = "us-central1"
+  impersonate_service_account = "jenkins-sa-cicd@devops-engineer-123.iam.gserviceaccount.com"
 }
